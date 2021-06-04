07fd0f90f3b6d1be53081718a08c6249a502a911
chao <quchao@seas.upenn.edu>
2021-06-03 20:37:56 -0400

    update cmake

--- cmake/Cache.cmake
+++ cmake/Cache.cmake
@@ -8,24 +8,19 @@ set(CACHE_OPTION
     CACHE STRING "Compiler cache to be used")
 set(CACHE_OPTION_VALUES "ccache" "sccache")
 set_property(CACHE CACHE_OPTION PROPERTY STRINGS ${CACHE_OPTION_VALUES})
-list(
-  FIND
-  CACHE_OPTION_VALUES
-  ${CACHE_OPTION}
-  CACHE_OPTION_INDEX)
+list(FIND CACHE_OPTION_VALUES ${CACHE_OPTION} CACHE_OPTION_INDEX)
 
 if(${CACHE_OPTION_INDEX} EQUAL -1)
   message(
     STATUS
-      "Using custom compiler cache system: '${CACHE_OPTION}', explicitly supported entries are ${CACHE_OPTION_VALUES}")
+      "Using custom compiler cache system: '${CACHE_OPTION}', explicitly supported entries are ${CACHE_OPTION_VALUES}"
+  )
 endif()
 
 find_program(CACHE_BINARY ${CACHE_OPTION})
 if(CACHE_BINARY)
   message(STATUS "${CACHE_OPTION} found and enabled")
   set(CMAKE_CXX_COMPILER_LAUNCHER ${CACHE_BINARY})
-  # add color
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
 else()
   message(WARNING "${CACHE_OPTION} is enabled but was not found. Not using it")
 endif()
